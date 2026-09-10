dde9a302267994bd9e5432831a02e71ca167a92f
Rizki Cahya <rizki.cahya03@gmail.com>
2026-09-11 01:54:13 +0700

    feat: navbar setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import "./App.css";
+import Navigation from "./components/Navigation";
 
 function App() {
-    return (
-        <div>
-            <h1>Jogja Culinary versi baru</h1>
-        </div>
-    );
+    return <Navigation />;
 }
 
 export default App;
