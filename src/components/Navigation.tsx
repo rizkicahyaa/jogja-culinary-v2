e1b38e7fc7bdce86a2611e74afb047ec4b2335d7
Rizki Cahya <rizki.cahya03@gmail.com>
2026-09-11 22:38:17 +0700

    feat: navbar finishing

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,36 +1,40 @@
 import "../App.css";
 
-import Button from "react-bootstrap/Button";
 import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import Button from "react-bootstrap/Button";
 
 function Navigation() {
     return (
-        <Navbar expand="lg" className="bg-body-tertiary">
+        <Navbar expand="lg" className="culinary-navbar">
             <Container>
-                <Navbar.Brand href="#">Jogja Culinary v2</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav className="me-auto my-2 my-lg-0" style={{ maxHeight: "100px" }} navbarScroll>
-                        <Nav.Link href="#action1">Home</Nav.Link>
-                        <Nav.Link href="#action2">Link</Nav.Link>
-                        <NavDropdown title="Link" id="navbarScrollingDropdown">
-                            <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action5">Something else here</NavDropdown.Item>
-                        </NavDropdown>
-                        <Nav.Link href="#" disabled>
-                            Link
+                <Navbar.Brand href="/" className="brand">
+                    Jogja Culinary
+                </Navbar.Brand>
+
+                <Navbar.Toggle aria-controls="main-navbar" />
+
+                <Navbar.Collapse id="main-navbar">
+                    <Nav className="mx-auto culinary-menu">
+                        <Nav.Link href="#home" className="active">
+                            Home
                         </Nav.Link>
+
+                        <Nav.Link href="#kuliner">Kuliner</Nav.Link>
+
+                        <Nav.Link href="#rekomendasi">Rekomendasi</Nav.Link>
+
+                        <Nav.Link href="#about">Tentang</Nav.Link>
                     </Nav>
-                    <Form className="d-flex">
-                        <Form.Control type="search" placeholder="Search" className="me-2" aria-label="Search" />
-                        <Button variant="outline-success">Search</Button>
-                    </Form>
+
+                    <div className="navbar-actions">
+                        <Button variant="light" className="search-btn">
+                            <i className="bi bi-search"></i>
+                        </Button>
+
+                        <Button className="login-btn">Login</Button>
+                    </div>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
